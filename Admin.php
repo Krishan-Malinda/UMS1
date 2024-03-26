d6645baed94c0bcb6f4177eabad14b373347baf6
Krishan-Malinda <krishanmalinda99@gmail.com>
2024-03-26 14:05:26 +0530

    Fix Login, make view admin details feature  and, include admin add feature

--- Admin.php
+++ Admin.php
@@ -9,42 +9,67 @@
 </head>
 <body>
 
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submitBtn'])) {
-    $user = $_POST["userId"];
-    $pwd = $_POST["password"];
+<?php
 
-    $query = "SELECT * FROM users WHERE users.id='$user' AND users.password='$pwd'";
-    $result = mysqli_query($conn, $query);
-    if (mysqli_num_rows($result)>0) {
-        foreach ($result as $re) {
+function welcomeAdmin($rel){
+    if (!(mysqli_num_rows($rel)>0)) {
+        header("Location:index.php?Login failed!");
+    }else{
+        foreach($rel as $re) {
             echo '<h3>Hello, '.$re['username'].'</h3>';
         }
     }
 }
 
-
-echo "<form action='#' method='POST'>";
-    echo "<button name='allUser'>View All Users</button>";
-echo "</form>";
-
-
-    if(isset($_POST['allUser'])){
-        $allUsers = mysqli_query($conn,"SELECT * FROM users");
-        if(mysqli_num_rows($allUsers)>0){
-            echo "<table>
-                    <tr>
-                        <td>User ID</td>
-                        <td>User Name</td>
-                    </tr>";
-            foreach($allUsers as $usrs){
-                echo "<tr>";
-                echo "<td>" . $usrs["id"] . "</td><td>" . $usrs["username"] . "</td>";
-                echo "</tr>";
-            }
-            echo "<table>";
+function viewResult($rel){
+    if (!(mysqli_num_rows($rel)>0)) {
+        echo 'There is no records!';
+    }else{
+        echo "<table>
+                <tr>
+                    <td>User ID</td>
+                    <td>User Name</td>
+                    <!-- <td>Password</td> -->
+                </tr>";
+        foreach($rel as $usrs){
+            echo "<tr>";
+            /* foreach($usrs as $value){
+                echo "<td>". $value."</td>";
+            } */
+            echo "<td>" . $usrs["id"] . "</td><td>" . $usrs["username"] . "</td>";
+            echo "</tr>";
         }
+        echo "<table>";
     }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submitBtn'])) {
+    $user = $_POST["username"];
+    $pwd = $_POST["password"];
+
+    $query = "SELECT * FROM users WHERE users.username='$user' AND users.password='$pwd'";
+    $result = mysqli_query($conn, $query);
+
+    welcomeAdmin($result);
+    
+    //echo "<form method='POST' action=".htmlspecialchars($_SERVER['PHP_SELF']).">";
+    echo "<form method='POST'>";
+        echo "<input type='submit' name='allUser' value='View admins'></input> &ensp;";
+        echo "<input type='submit' name='addAdmin' value='Add Admin'></input>";
+    echo "</form>";
+    
+}
+
+if(isset($_POST['allUser'])){
+    $allUsers = mysqli_query($conn,"SELECT * FROM users");
+    viewResult($allUsers);
+}
+
+if(isset($_POST['addAdmin'])){
+    header("Location:AddAdmin.php");
+}
+
 ?>
 
 </body>
